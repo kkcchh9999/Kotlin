f54771e989e9dffde318ef967240c0c871a8ee97
권철현 <kkcchh9999@naver.com>
2021-04-29 15:09:30 +0900

    챕터 1

---
+++ src/main/kotlin/HelloKotlin.kt
@@ -0,0 +1,6 @@
+fun main(args: Array<String>) {
+    println("Hello Kotlin!")
+}
+
+//JVM 에서 실행되는 코틀린 코드는 main 함수를 기준으로 자바 클래스 자동 생성
+//Tools > Kotlin > Show Kotlin Bytecode > Decompile 에서 확인 가능
---
+++ src/main/kotlin/MainArgs.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    println(args[0])
+    println(args[1])
+    println(args[2])
+}
+//메인의 인자 사용하기.
+//RUN > Edit Configurations > Program Arguments 에서 설정 가능
